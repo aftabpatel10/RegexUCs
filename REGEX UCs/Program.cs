2410101984046d1a0aaa4e5b20e564604c751d92
Patel <aftabkpatel10@gmail.com>
2022-12-09 11:56:00 +0530

    Setting a Password With Atleast 1 Numeric Number

--- REGEX UCs/Program.cs
+++ REGEX UCs/Program.cs
@@ -20,7 +20,8 @@ namespace REGEX_UCs
                 "Press 3: To Register your EmailID\n" +
                 "Press 4: to enter your Mobile Number.\n" +
                 "Press 5: To set your password (minimum 8 characters) \n" +
-                "Press 6: To set password having 8 minimum character and atleast 1 upper case in it .\n");
+                "Press 6: To set password having 8 minimum character and atleast 1 upper case in it .\n" +
+                "Press 7: To set password having 8 min char and atleast 1 upper case and 1 numeric in it.\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -41,6 +42,9 @@ namespace REGEX_UCs
                         break;
                     case 6:
                         UserDetails.PassMin8CharAnd1UpperCase();
+                        break;
+                    case 7:
+                        UserDetails.UpperCase1AndNumeric1();
                         break;
                     default:
                         Console.WriteLine("Invalid option selected ,Please try again !");
