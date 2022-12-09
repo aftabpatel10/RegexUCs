e2cf2c8f15046c535ef0a7154b9088de7e020183
Patel <aftabkpatel10@gmail.com>
2022-12-09 09:51:34 +0530

    Adding Name Of The User

---
+++ REGEX UCs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static REGEX_UCs.UserRegDetails;
+
+namespace REGEX_UCs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration using Regex.\n");
+            while (true)
+            {
+                Console.WriteLine("\nPlease select any one from below :\n" +
+                "Press 1: To Register your First Name.\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        UserDetails.FirstName();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option selected ,Please try again !");
+                        break;
+                }
+            }
+        }
+    }
+    
+}
---
+++ REGEX UCs/UserRegDetails.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace REGEX_UCs
+{
+    public class UserRegDetails
+    {
+        public class UserDetails
+        {
+            public static void Iteration(string userInput, string regexCondition)
+            {
+                if (Regex.IsMatch(userInput, regexCondition))
+                {
+                    Console.WriteLine("Registered Successfully!\n");
+                }
+                else
+                {
+                    Console.WriteLine("Entered Details are not in required format.Please try again!\n");
+                }
+            }
+            //UC1
+            public static void FirstName()  
+            {
+                Console.WriteLine("Enter your FirstName");
+                string userInput = Console.ReadLine();
+                string regexCondition = "^[A-Z]{1}[a-z]{3,}$";
+                Iteration(userInput, regexCondition);
+            }
+           
+        }
+    }
+}
